4523d2857a2e197b589c616de8510729c05a333a
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-05-11 14:30:07 +0500

    add studets number

--- apps/main/center/admin.py
+++ apps/main/center/admin.py
@@ -9,9 +9,9 @@ from apps.main.center.models import Center
 
 @admin.register(Center)
 class CenterAdmin(TranslationAdmin):
-    list_display = ['id', 'name', 'main_course', 'is_public', 'score']
+    list_display = ['id', 'name', 'main_course', 'is_public', 'score', 'number_students']
     list_display_links = ['name', 'main_course']
-    list_filter = ['name', 'main_course', 'is_public']
+    list_filter = ['name', 'main_course', 'is_public', 'number_students']
     # readonly_fields = ['get_image']
 
     # def get_image(self, obj):
