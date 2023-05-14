8741e7038cb88f3bfa8ad2d54ca1e9ef11d543a1
Abduraxmonnn <abduraxmonasatullayev35@gmail.com>
2023-05-14 13:04:33 +0500

    sign

--- apps/user/api/__init__.py
+++ apps/user/api/__init__.py
@@ -1,4 +1,5 @@
 from apps.user.api.get_token import UserTokenAPIView
+from apps.user.api.login import UserLogInView
 from apps.user.api.singup import UserSignUpAPIView
 from apps.user.api.verify_account import VerifyOTPAPIView
 from apps.user.api.userme import UserMeView
